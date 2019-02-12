83b38fb58d07f39dbac31ca8b4a088edb1927631
Zooheck <davidscottflack@gmail.com>
2019-02-12 17:31:26 -0600

    add cors and react app

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
+const cors = require('cors')
 const postsRouter = require('./data/post-router.js')
 
 const server = express();
 server.use(express.json());
-
+server.use(cors())
 server.use('/api/posts', postsRouter)
 
 module.exports = server;
